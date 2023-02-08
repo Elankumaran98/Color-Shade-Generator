819af59a3e87a9f254f597ada74f4cf17a622eea
Elankumaran98 <siva98kumarane@gmail.com>
2023-02-08 10:43:39 +0530

    updated

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,39 @@
-import './App.css';
-import Values from 'values.js'
-import ChromeColor from '@uiw/react-color-chrome';
-import {CopyToClipboard} from 'react-copy-to-clipboard';
+import "./App.css";
+import { useState } from "react";
+import Values from "values.js";
+import ChromeColor from "@uiw/react-color-chrome";
+import { CopyToClipboard } from "react-copy-to-clipboard";
 
 function App() {
+  const [color, setColor] = useState("#ccc");
+  const [shades, setShades] = useState([]);
+  const onChange = (color) => {
+    setColor(color.hex);
+    const colorValues = new Values(color.hex);
+    const newColors = colorValues.all(10);
+    newColors.pop();
+    setShades(newColors);
+    console.log(newColors);
+  };
   return (
-    <div className="App">
-      Laxiga
+    <div className="wrapper">
+      <h1 className="heading">Shades Generator ({color})</h1>
+      <div className="color-picker">
+        <ChromeColor color={color} onChange={onChange} />
+      </div>
+      <div className="shades">
+        <ul className="shades-list">
+          {shades.map((shade) => {
+            return (
+              <CopyToClipboard text={'#'+shade.hex}>
+                <li style={{ backgroundColor: "#" + shade.hex }}>
+                  #{shade.hex}
+                </li>
+              </CopyToClipboard>
+            );
+          })}
+        </ul>
+      </div>
     </div>
   );
 }
